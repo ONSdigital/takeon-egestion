9ea41ef1f5fef4fe1f8d87b4bcaed68ed7f56d53
dllmorgan <github@ghost.me.uk>
2021-03-26 13:32:50 +0000

    Enable xray and set lambda cpu/memory

--- aws-infrastructure/egest.tf
+++ aws-infrastructure/egest.tf
@@ -155,15 +155,23 @@ resource "aws_lambda_function" "export" {
   handler = "bin/main"
   runtime = "go1.x"
   filename = data.archive_file.dummy.output_path
+  memory_size = 1024
+
+   tracing_config {
+    mode = "Active"
+  }
+
   vpc_config {
     subnet_ids = [data.aws_subnet.private-subnet.id, data.aws_subnet.private-subnet2.id]
     security_group_ids = [data.aws_security_group.vpc-private.id]
   }
+
   environment {
     variables = {
-        GRAPHQL_ENDPOINT: "http://${data.aws_lb.business-layer.dns_name}/contributor/dbExport",
-        S3_BUCKET       : aws_s3_bucket.export.id
-        DB_EXPORT_OUTPUT_QUEUE: "${local.name_prefix}-db-export-output"
+        GRAPHQL_ENDPOINT       : "http://${data.aws_lb.business-layer.dns_name}/contributor/dbExport",
+        S3_BUCKET              : aws_s3_bucket.export.id
+        DB_EXPORT_OUTPUT_QUEUE : "${local.name_prefix}-db-export-output"
+        LOG_LEVEL              : "INFO"
     }
   }
 
