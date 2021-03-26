a89e81e5729619ab912065b350858eacc8932401
dllmorgan <github@ghost.me.uk>
2021-03-26 13:07:15 +0000

    Add encryption and add extra non-public access rules

--- aws-infrastructure/egest.tf
+++ aws-infrastructure/egest.tf
@@ -119,15 +119,33 @@ data "archive_file" "dummy" {
 resource "aws_s3_bucket" "export" {
   bucket = "${local.name_prefix}-export"
   acl    = "private"
+  
+  force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = merge(
-      var.common_tags,
-      {
-      Name = "${local.name_prefix}-export",
-      "ons:name" = "${local.name_prefix}-export"
-      }
+    var.common_tags,
+    {
+    Name = "${local.name_prefix}-export",
+    "ons:name" = "${local.name_prefix}-export"
+    }
   )
 }
+
+resource "aws_s3_bucket_public_access_block" "export" {
+  bucket = aws_s3_bucket.export.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+  
 
 
 # Export lambda
