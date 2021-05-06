ab0347896710984534e2c6d233293e6ee70303ea
dllmorgan <github@ghost.me.uk>
2021-05-06 17:39:51 +0100

    Update private subnet usage

--- aws-infrastructure/data.tf
+++ aws-infrastructure/data.tf
@@ -1,27 +1,20 @@
 # Importing existing resources for lambdas to use
+data "aws_caller_identity" "current" {}
 data "aws_security_group" "vpc-private" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${local.vpc_prefix}-vpc-private"]
   }
 }
 
-data "aws_subnet" "private-subnet" {
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.main.id
   filter {
-    name = "tag:Name"
-    values = ["${local.vpc_prefix}-vpc-private-subnet"]
-  }
-}
-
-data "aws_subnet" "private-subnet2" {
-  filter {
-    name = "tag:Name"
-    values = ["${local.vpc_prefix}-vpc-private-subnet2"]
+    name   = "tag:Name"
+    values = ["${local.name_prefix}-private*"]
   }
 }
 
 data "aws_lb" "business-layer" {
-    name = "${local.vpc_prefix}-bl"
+  name = "${local.vpc_prefix}-ecs-app-loadbalancer"
 }
-
-data "aws_caller_identity" "current" {}
--- aws-infrastructure/lambda.tf
+++ aws-infrastructure/lambda.tf
@@ -1,47 +1,47 @@
 # Dummy content to provision temporary lambdas
 data "archive_file" "dummy" {
-    type = "zip"
-    output_path = "lambda_function_payload.zip"
+  type        = "zip"
+  output_path = "lambda_function_payload.zip"
 
-    source {
-        content = "hello"
-        filename = "dummy.txt"
-    }
+  source {
+    content  = "hello"
+    filename = "dummy.txt"
+  }
 }
 
-# Export lambda
+# Export lambda01
 resource "aws_lambda_function" "export" {
   function_name = "${local.name_prefix}-export"
-  role = aws_iam_role.lambda.arn
-  handler = "bin/main"
-  runtime = "go1.x"
-  filename = data.archive_file.dummy.output_path
-  timeout     = 300
-  memory_size = 1024
+  role          = aws_iam_role.lambda.arn
+  handler       = "bin/main"
+  runtime       = "go1.x"
+  filename      = data.archive_file.dummy.output_path
+  timeout       = 300
+  memory_size   = 1024
 
-   tracing_config {
+  tracing_config {
     mode = "Active"
   }
 
   vpc_config {
-    subnet_ids = [data.aws_subnet.private-subnet.id, data.aws_subnet.private-subnet2.id]
+    subnet_ids         = data.aws_subnet_ids.private
     security_group_ids = [data.aws_security_group.vpc-private.id]
   }
 
   environment {
     variables = {
-        GRAPHQL_ENDPOINT       : "http://${data.aws_lb.business-layer.dns_name}/contributor/dbExport",
-        S3_BUCKET              : aws_s3_bucket.export.id
-        DB_EXPORT_OUTPUT_QUEUE : "${local.name_prefix}-db-export-output"
-        LOG_LEVEL              : "INFO"
+      GRAPHQL_ENDPOINT : "http://${data.aws_lb.business-layer.dns_name}/contributor/dbExport",
+      S3_BUCKET : aws_s3_bucket.export.id
+      DB_EXPORT_OUTPUT_QUEUE : "${local.name_prefix}-db-export-output"
+      LOG_LEVEL : "INFO"
     }
   }
 
   tags = merge(
     var.common_tags,
     {
-    Name = "${local.name_prefix}-export",
-    "ons:name" = "${local.name_prefix}-export"
+      Name       = "${local.name_prefix}-export",
+      "ons:name" = "${local.name_prefix}-export"
     }
   )
 }
